17e4a81f9996083c34299c9b6cc5061f5128b5cc
mountain <pawer02023@gmail.com>
2023-05-13 20:32:11 +0900

    Feat/4_ci_setting (#6)
    * Feat/4 add project setting (#5)
    
    * feat: multimodule 구조 변경
    
    web, api 모듈 통합해 api 모듈 하나로 구성 (api 서버)
    전체 api, domain, infra로 모듈 구성
    
    * feat: 플러그인 추가 및 수정
    
    all open 플러그인 추가
    플러그인 모듈에 맞춰 위치 조정
    
    * feat: code owners 설정 추가
    
    * ci default 적용
    
    - ci default 적용
    
    * feat: ci 스크립트 이름 변경
    
    - ci 스크립트 이름 변경
    
    ---------
    
    Co-authored-by: 이채린 (Bryn) <67696767+cofls6581@users.noreply.github.com>

--- web1-api/src/main/kotlin/com/yapp/web1be/test/TestController.kt
+++ web1-api/src/main/kotlin/com/yapp/web1be/test/TestController.kt
@@ -4,11 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TestController( private val testService: TestService) {
-
+class TestController(private val testService: TestService) {
     @GetMapping
     fun test(): TestEntity {
         return testService.test()
     }
-
 }
