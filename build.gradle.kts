db2c44c07dc5550382c7aaa2d889e7d5d5a80027
taesan <pawer02023@gmail.com>
2023-05-14 18:30:02 +0900

    feat: kotest, mockk 추가
    - kotest, mockk 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val kotestVersion = "5.5.4"
+
 plugins {
     id("org.springframework.boot") version "2.7.11" apply false
     id("io.spring.dependency-management") version "1.0.15.RELEASE" apply false
@@ -44,5 +46,11 @@ subprojects {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
 
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+        testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+        testImplementation("io.kotest:kotest-framework-datatest:$kotestVersion")
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+        testImplementation("io.mockk:mockk:1.12.4")
+        testImplementation("com.ninja-squad:springmockk:3.1.2")
     }
 }
---
+++ web1-api/src/test/kotlin/com/yapp/web1be/test/TestServiceTest.kt
@@ -0,0 +1,19 @@
+package com.yapp.web1be.test
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+
+class TestServiceTest : StringSpec({
+    val testService:TestService = mockk()
+
+    "kotest 테스트 " {
+        // given
+        every { testService.test() } returns TestEntity()
+        val result = testService.test()
+
+        result.id shouldBe 1
+        result.name shouldBe "test"
+    }
+})
