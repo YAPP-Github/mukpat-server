77814e4b63e7a0b4eafbee9f3b55a904783ab000
taesan <pawer02023@gmail.com>
2023-05-18 22:26:26 +0900

    refactor: ktlint plugin 적용
    - ktlint 적용되지 않아 plugins에 추가 함

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter-data-jpa")
