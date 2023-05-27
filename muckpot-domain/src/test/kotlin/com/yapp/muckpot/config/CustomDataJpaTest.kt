9b8872be4fb618d6f13c7aec29ca79156cf163ad
taesan <pawer02023@gmail.com>
2023-05-27 11:33:55 +0900

    refactor: 어노테이션 중복 Retention 선언 제거
    - @DataJpaTest에 포함되어 제거

--- muckpot-domain/src/test/kotlin/com/yapp/muckpot/config/CustomDataJpaTest.kt
+++ muckpot-domain/src/test/kotlin/com/yapp/muckpot/config/CustomDataJpaTest.kt
@@ -3,7 +3,6 @@ package com.yapp.muckpot.config
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
-@Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
