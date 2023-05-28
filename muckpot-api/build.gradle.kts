16e169084f24fb5f55da2e7e41cafd0e2b326e9c
이채린 (Bryn) <67696767+cofls6581@users.noreply.github.com>
2023-05-28 20:15:15 +0900

    feat : 30 elasticache 연동 (#31)
    * feat: elasticache 연동
    
    - elasticache 연동
    - redis test code 작성
    - local, dev redis 설정 분리
    
    * refactor: 필요없는 코드 제거
    
    * refactor: redisConfig 코드 리팩토링
    
    * fix: 프로필 못잡는 에러 수정

--- muckpot-api/build.gradle.kts
+++ muckpot-api/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":muckpot-domain"))
+    implementation(project(":muckpot-infra"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
