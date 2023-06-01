d91975e460665471f9505497d4bc022b4a5514fb
taesan <pawer02023@gmail.com>
2023-06-02 00:56:28 +0900

    feat: 인증 및 인가 실패시 예외 처리
    - 인증 및 인가 실패시 예외 처리
    - 인증되지 않은 경우에도 체크가 필요하여 EntryPoint 구현
      - AccessDeniedHandler 는 인증이 안되었으면 타지않음.

---
+++ muckpot-api/src/main/kotlin/com/yapp/muckpot/common/security/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,26 @@
+package com.yapp.muckpot.common.security
+
+import com.yapp.muckpot.common.ResponseWriter
+import mu.KLogging
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class CustomAuthenticationEntryPoint : AuthenticationEntryPoint {
+    private val log = KLogging().logger
+
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException
+    ) {
+        log.debug(authException) { NO_AUTH }
+        ResponseWriter.writeResponse(response, HttpStatus.FORBIDDEN, NO_AUTH)
+    }
+
+    companion object {
+        private const val NO_AUTH = "권한이 존재하지 않습니다."
+    }
+}
