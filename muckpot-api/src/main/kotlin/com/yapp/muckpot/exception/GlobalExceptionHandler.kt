e222b8050451ff00cd4ec2a0abc7b9bf64ac870a
bryn <cofls6581@naver.com>
2023-06-01 14:10:50 +0900

    feat: MethodArgumentNotValidException 에러 핸들링

--- muckpot-api/src/main/kotlin/com/yapp/muckpot/exception/GlobalExceptionHandler.kt
+++ muckpot-api/src/main/kotlin/com/yapp/muckpot/exception/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.yapp.muckpot.exception
 import com.yapp.muckpot.common.ResponseDto
 import org.springframework.http.HttpStatus.valueOf
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -12,6 +13,23 @@ class GlobalExceptionHandler {
     @ExceptionHandler(MuckPotException::class)
     fun muckpotGlobalExceptionHandler(e: MuckPotException): ResponseEntity<ResponseDto> {
         val responseDto = e.errorCode.toResponseDto()
+        return ResponseEntity.status(valueOf(responseDto.status)).body(responseDto)
+    }
+
+    // javax.validation.constraints 오류 (dto validation 오류)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ResponseDto> {
+        // TODO: validation 여러 개에 어긋날 경우 메시지를 한 번에 다 보내야할지? ex. "message": "삼성만 가능","전화번호 입력 필수"
+//      val bindingResult: BindingResult = e.bindingResult
+        val error = e.bindingResult.fieldError?.defaultMessage
+//        val errors = bindingResult.allErrors.map { error ->
+//            when (error) {
+//                is FieldError -> error.defaultMessage.toString()
+//                else -> "잘못된 요청입니다."
+//            }
+//        }
+//        val formattedErrors = errors.joinToString("\", \"").removeSurrounding("[", "]")
+        val responseDto = ResponseDto(400, error, null)
         return ResponseEntity.status(valueOf(responseDto.status)).body(responseDto)
     }
 }
--- muckpot-api/src/main/kotlin/com/yapp/muckpot/user/dto/SendEmailAuthRequest.kt
+++ muckpot-api/src/main/kotlin/com/yapp/muckpot/user/dto/SendEmailAuthRequest.kt
@@ -7,9 +7,10 @@ import javax.validation.constraints.Pattern
 
 @ApiModel(value = "이메일 인증 요청 Request")
 data class SendEmailAuthRequest(
+    // TODO: 개발 서버에서 메일 정상 작동 확인 후 삼성전자로 변경 필요!
     // @field:Pattern(regexp = "^[A-Za-z0-9._%+-]+@samsung\\.com\$", message = "현재 버전은 삼성전자 사우만 이용 가능합니다.")
     @field:ApiModelProperty(notes = "이메일", required = true, example = "co@samsung.com")
     @field:NotBlank(message = "이메일을 입력해 주세요.")
-    @field:Pattern(regexp = "^[A-Za-z0-9._%+-]+@naver\\.com\$", message = "현재 버전은 삼성전자 사우만 이용 가능합니다.")
+    @field:Pattern(regexp = "^[A-Za-z0-9._%+-]+@naver\\.com\$", message = "현재 버전은 네이버 사우만 이용 가능합니다.") // for test
     var email: String?
 )
