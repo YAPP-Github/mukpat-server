91168542ff48ce8d7b42a8419a6b2556cdff99c3
mountain <pawer02023@gmail.com>
2023-06-03 16:22:13 +0900

    fix: 비로그인 유저 가능 api 추가 (#39)
    - 회원가입, 이메일 인증, 이메일 전송

--- muckpot-api/src/main/kotlin/com/yapp/muckpot/common/JwtConstant.kt
+++ muckpot-api/src/main/kotlin/com/yapp/muckpot/common/JwtConstant.kt
@@ -1,3 +1,6 @@
 package com.yapp.muckpot.common
 
 const val LOGIN_URL = "/api/v1/users/login"
+const val SIGN_UP_URL = "/api/v1/users"
+const val EMAIL_REQUEST = "/api/v1/emails/request"
+const val EMAIL_VERIFY = "/api/v1/emails/verify"
