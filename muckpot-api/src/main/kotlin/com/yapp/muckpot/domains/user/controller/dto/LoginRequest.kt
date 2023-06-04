db11035194300ae59aa490ff42686f1d4a5d9be7
taesan <pawer02023@gmail.com>
2023-06-04 21:47:16 +0900

    fix: 상수 사용되는곳 변경
    - 상수 사용되는곳 변경 및 테스트 간단한 수정

--- muckpot-api/src/main/kotlin/com/yapp/muckpot/domains/user/controller/dto/LoginRequest.kt
+++ muckpot-api/src/main/kotlin/com/yapp/muckpot/domains/user/controller/dto/LoginRequest.kt
@@ -1,5 +1,7 @@
 package com.yapp.muckpot.domains.user.controller.dto
 
+import com.yapp.muckpot.common.ONLY_NAVER
+import com.yapp.muckpot.common.PASSWORD
 import com.yapp.muckpot.common.enums.YesNo
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
@@ -7,12 +9,13 @@ import javax.validation.constraints.Pattern
 
 @ApiModel(value = "로그인 요청")
 data class LoginRequest(
-    @field:ApiModelProperty(notes = "이메일", required = true, example = "user@samsung.com")
-    @field:Pattern(regexp = "^[A-Za-z0-9._%+-]+@samsung\\.com$", message = "현재 버전은 삼성전자 사우만 이용 가능합니다.")
+    // TODO 상용 시 samsung.com 으로 변경
+    @field:ApiModelProperty(notes = "이메일", required = true, example = "user@naver.com")
+    @field:Pattern(regexp = ONLY_NAVER, message = "현재 버전은 삼성전자 사우만 이용 가능합니다.")
     val email: String,
     @field:ApiModelProperty(notes = "비밀번호", required = true, example = "abcd1234")
     @field:Pattern(
-        regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,20}$",
+        regexp = PASSWORD,
         message = "비밀 번호는 영문과 숫자를 포함하여 8 ~ 20자로 입력해 주세요."
     )
     val password: String,
