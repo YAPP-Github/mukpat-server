7199701f3e6aa0b3bf11e3fca52cbc08ac0ad065
taesan <pawer02023@gmail.com>
2023-06-04 22:04:50 +0900

    fix: 소나큐브 실패 처리 2
    - cause: "PASSWORD" detected here, make sure this is not a hard-coded credential.

--- muckpot-domain/src/main/kotlin/com/yapp/muckpot/common/RegexPatternConst.kt
+++ muckpot-domain/src/main/kotlin/com/yapp/muckpot/common/RegexPatternConst.kt
@@ -2,7 +2,7 @@ package com.yapp.muckpot.common
 
 const val EMAIL = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
 const val MEETING_TIME = "^(오전|오후) (0[1-9]|1[0-2]):([0-5][0-9])$"
-const val PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,20}\$"
+const val PW_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,20}\$"
 
 const val ONLY_NAVER = "^[A-Za-z0-9._%+-]+@naver\\.com\$"
 const val YYYYMMDD = "yyyy-MM-dd"
