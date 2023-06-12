41c98b498b4207496702211a8eef9eee6f811c3e
bryn <cofls6581@naver.com>
2023-06-13 03:52:45 +0900

    test: 쓰레드풀 500 테스트

--- muckpot-domain/src/test/kotlin/com/yapp/muckpot/common/redisson/ConcurrencyHelper.kt
+++ muckpot-domain/src/test/kotlin/com/yapp/muckpot/common/redisson/ConcurrencyHelper.kt
@@ -6,8 +6,8 @@ import java.util.concurrent.atomic.AtomicLong
 
 class ConcurrencyHelper {
     companion object {
-        private const val NUMBER_OF_THREADS = 100
-        private const val NUMBER_OF_THREAD_POOL = 100
+        private const val NUMBER_OF_THREADS = 500
+        private const val NUMBER_OF_THREAD_POOL = 500
 
         fun execute(operation: () -> Any?, successCount: AtomicLong) {
             val executorService = Executors.newFixedThreadPool(NUMBER_OF_THREAD_POOL)
--- muckpot-domain/src/test/kotlin/com/yapp/muckpot/common/redisson/DistributedLockAopTest.kt
+++ muckpot-domain/src/test/kotlin/com/yapp/muckpot/common/redisson/DistributedLockAopTest.kt
@@ -2,8 +2,6 @@ package com.yapp.muckpot.common.redisson
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.stereotype.Service
@@ -20,7 +18,6 @@ class DistributedLockAopTest @Autowired constructor(
             { redissonService.test(1) },
             successCount
         )
-        println(redissonService.apply.toString() + " " + successCount.get())
         redissonService.apply shouldBe successCount.toInt()
     }
 })
@@ -32,37 +29,5 @@ class RedissonService(
     // @DistributedLock("id", "testLock")
     fun test(id: Int) {
         apply += 1
-        println(apply)
-    }
-}
-
-class RedissonAopTests {
-
-    var orderService = OrderService()
-
-    @Test
-    fun `분산락 적용시 동시요청에 올바르게 재고가 감소해야한다`() {
-        // given
-        // when
-        val successCount = AtomicLong()
-        ConcurrencyHelper.execute(
-            { orderService.decreaseStock(1) },
-            successCount
-        )
-        // then
-
-        val remain = 100 - successCount.toInt()
-        assertThat(orderService.stock).isEqualTo(remain)
-    }
-}
-
-// @Service
-class OrderService(
-    var stock: Int = 100
-) {
-
-    // @RedissonLock("id", "stockLock")
-    fun decreaseStock(id: Int) {
-        stock -= 1
     }
 }
