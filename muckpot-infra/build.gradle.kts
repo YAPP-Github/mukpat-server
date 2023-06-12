6641d7da962c207ba1a92802a593c88af382f24d
bryn <cofls6581@naver.com>
2023-06-12 20:20:33 +0900

    chore: setting redisson config

--- muckpot-infra/build.gradle.kts
+++ muckpot-infra/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.redisson:redisson:3.20.0")
 }
 
 tasks {
