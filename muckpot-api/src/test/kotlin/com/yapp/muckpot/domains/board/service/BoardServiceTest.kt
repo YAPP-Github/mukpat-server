4962c9bbdd175d56a4df063d73d6b9619923c7e6
bryn <cofls6581@naver.com>
2023-06-15 02:44:01 +0900

    test: 먹팟 참가 신청 api 테스트 코드

--- muckpot-api/src/test/kotlin/com/yapp/muckpot/domains/board/service/BoardServiceTest.kt
+++ muckpot-api/src/test/kotlin/com/yapp/muckpot/domains/board/service/BoardServiceTest.kt
@@ -2,14 +2,18 @@ package com.yapp.muckpot.domains.board.service
 
 import com.yapp.muckpot.common.Location
 import com.yapp.muckpot.common.enums.Gender
+import com.yapp.muckpot.common.redisson.ConcurrencyHelper
 import com.yapp.muckpot.domains.board.controller.dto.MuckpotCreateRequest
 import com.yapp.muckpot.domains.board.entity.ParticipantId
+import com.yapp.muckpot.domains.board.exception.BoardErrorCode
 import com.yapp.muckpot.domains.board.repository.BoardRepository
 import com.yapp.muckpot.domains.board.repository.ParticipantRepository
 import com.yapp.muckpot.domains.user.controller.dto.UserResponse
 import com.yapp.muckpot.domains.user.entity.MuckPotUser
 import com.yapp.muckpot.domains.user.enums.JobGroupMain
 import com.yapp.muckpot.domains.user.repository.MuckPotUserRepository
+import com.yapp.muckpot.exception.MuckPotException
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
@@ -18,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.repository.findByIdOrNull
 import java.time.LocalDate
 import java.time.LocalTime
+import java.util.concurrent.atomic.AtomicLong
 
 @SpringBootTest
 class BoardServiceTest @Autowired constructor(
@@ -31,7 +36,7 @@ class BoardServiceTest @Autowired constructor(
     val request = MuckpotCreateRequest(
         meetingDate = LocalDate.now(),
         meetingTime = LocalTime.of(12, 0),
-        maxApply = 10,
+        maxApply = 70,
         minAge = 20,
         maxAge = 100,
         locationName = "location",
@@ -85,5 +90,56 @@ class BoardServiceTest @Autowired constructor(
         // then
         val findBoard = boardRepository.findByIdOrNull(boardId)!!
         findBoard.views shouldBe 2
+    }
+
+    "먹팟 참가 신청 성공" {
+        // given
+        val boardId = boardService.saveBoard(userId, request)!!
+        val applyUser = userRepository.save(
+            MuckPotUser(
+                null, "test1@naver.com", "pw", "nickname1",
+                Gender.MEN, 2000, JobGroupMain.DEVELOPMENT, "sub", Location("location", 0.0, 0.0), "url"
+            )
+        )
+        val applyUserId = applyUser.id!!
+
+        // when
+        boardService.joinBoard(applyUserId, boardId)
+
+        // then
+        val findBoard = boardRepository.findByIdOrNull(boardId)!!
+        val participant = participantRepository.findByIdOrNull(ParticipantId(applyUser, findBoard))
+        findBoard.currentApply shouldBe 2
+        participant shouldNotBe null
+    }
+
+    "먹팟 중복 참가 신청 불가 검증" {
+        val boardId = boardService.saveBoard(userId, request)!!
+        shouldThrow<MuckPotException> {
+            boardService.joinBoard(userId, boardId)
+        }.errorCode shouldBe BoardErrorCode.ALREADY_JOIN
+    }
+
+    "먹팟 참가 동시성 테스트" {
+        // given
+        val boardId = boardService.saveBoard(userId, request)!!
+        val applyUser = userRepository.save(
+            MuckPotUser(
+                null, "test1@naver.com", "pw", "nickname1",
+                Gender.MEN, 2000, JobGroupMain.DEVELOPMENT, "sub", Location("location", 0.0, 0.0), "url"
+            )
+        )
+        val applyId = applyUser.id!!
+
+        // when
+        val successCount = AtomicLong()
+        ConcurrencyHelper.execute(
+            { boardService.joinBoard(applyId, boardId) },
+            successCount
+        )
+
+        // then
+        val findBoard = boardRepository.findByIdOrNull(boardId)!!
+        findBoard.currentApply shouldBe 2
     }
 })
