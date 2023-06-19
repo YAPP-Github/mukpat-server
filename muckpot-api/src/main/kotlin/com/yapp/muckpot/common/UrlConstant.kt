46f42bfd79725c4772ba11925d121395afdb267d
taesan <pawer02023@gmail.com>
2023-06-19 22:52:22 +0900

    refactor: 상수 파일 분리 - URL 관련 상수 따로 분리

---
+++ muckpot-api/src/main/kotlin/com/yapp/muckpot/common/UrlConstant.kt
@@ -0,0 +1,9 @@
+package com.yapp.muckpot.common
+
+const val LOGIN_URL = "/api/v1/users/login"
+const val SIGN_UP_URL = "/api/v1/users"
+const val EMAIL_REQUEST_URL = "/api/v1/emails/request"
+const val EMAIL_VERIFY_URL = "/api/v1/emails/verify"
+const val USER_PROFILE_URL = "/v1/users/profile"
+const val LOGOUT_URL = "/api/v1/users/logout"
+const val ROOT_URL = "/"
