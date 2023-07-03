a89326a688efb4ec076ddfee9ab640b666e73470
bryn <cofls6581@naver.com>
2023-07-04 02:46:07 +0900

    feat: AWS SES 연동

---
+++ muckpot-infra/src/main/kotlin/com/yapp/muckpot/email/AwsSesService.kt
@@ -0,0 +1,41 @@
+package com.yapp.muckpot.email
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService
+import com.amazonaws.services.simpleemail.model.Body
+import com.amazonaws.services.simpleemail.model.Content
+import com.amazonaws.services.simpleemail.model.Destination
+import com.amazonaws.services.simpleemail.model.Message
+import com.amazonaws.services.simpleemail.model.SendEmailRequest
+import mu.KLogging
+import org.springframework.mail.MailException
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Service
+
+@Service
+class AwsSesService(
+    private val amazonSimpleEmailService: AmazonSimpleEmailService
+) {
+    private val log = KLogging().logger
+
+    @Async
+    fun sendAuthMail(authKey: String, to: String) {
+        try {
+            val subject = EmailTemplates.AUTH_EMAIL_SUBJECT
+            val text = EmailTemplates.AUTH_EMAIL_TEXT
+            val textSetting = text.formatText(authKey)
+
+            val message = Message()
+            message.withSubject(Content(subject))
+            message.withBody(Body().withHtml(Content(textSetting)))
+            val destination = Destination().withToAddresses(to)
+            amazonSimpleEmailService.sendEmail(
+                SendEmailRequest()
+                    .withSource("noreply@mukpat.com")
+                    .withDestination(destination)
+                    .withMessage(message)
+            )
+        } catch (e: MailException) {
+            log.error { "Failed to send email: ${e.message}" }
+        }
+    }
+}
