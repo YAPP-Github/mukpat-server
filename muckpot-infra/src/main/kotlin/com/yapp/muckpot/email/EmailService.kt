80a3920509b4ba7b584cb2907e5a6a323c098689
이채린 (Bryn) <67696767+cofls6581@users.noreply.github.com>
2023-07-05 00:15:15 +0900

    feat :  이메일 서버 AWS SES로 변경 및 적용 (#93)
    * feat: AWS SES 연동
    
    * refactor: 이메일 인증 요청 API 메일 서버 변경
    
    -google smtp에서 aws ses로
    
    * refactor: google smtp 관련 파일 제거
    
    * refactor: 이메일 전송 함수 인터페이스화

--- muckpot-infra/src/main/kotlin/com/yapp/muckpot/email/EmailService.kt
+++ muckpot-infra/src/main/kotlin/com/yapp/muckpot/email/EmailService.kt
@@ -1,33 +1,5 @@
 package com.yapp.muckpot.email
-import mu.KLogging
-import org.springframework.mail.MailException
-import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.mail.javamail.MimeMessageHelper
-import org.springframework.scheduling.annotation.Async
-import org.springframework.stereotype.Service
 
-@Service
-class EmailService(private val javaMailSender: JavaMailSender) {
-
-    private val log = KLogging().logger
-
-    @Async
-    fun sendAuthMail(authKey: String, to: String) {
-        try {
-            val subject = EmailTemplates.AUTH_EMAIL_SUBJECT
-            val text = EmailTemplates.AUTH_EMAIL_TEXT
-            val textSetting = text.formatText(authKey)
-
-            val email = javaMailSender.createMimeMessage()
-            val helper = MimeMessageHelper(email, true, "UTF-8")
-            helper.setTo(to)
-            helper.setFrom("muckpotinfo@gmail.com")
-            helper.setTo(to)
-            helper.setSubject(subject)
-            helper.setText(textSetting, true)
-            javaMailSender.send(email)
-        } catch (e: MailException) {
-            log.error { "Failed to send email: ${e.message}" }
-        }
-    }
+interface EmailService {
+    fun sendAuthMail(authKey: String, to: String)
 }
--- muckpot-infra/src/testFixtures/kotlin/config/InfraProperties.kt
+++ muckpot-infra/src/testFixtures/kotlin/config/InfraProperties.kt
@@ -2,9 +2,8 @@ package config
 
 object InfraProperties {
     init {
-        System.setProperty("spring.mail.host", "smtp.gmail.com")
-        System.setProperty("spring.mail.port", "8080")
-        System.setProperty("spring.mail.username", "test")
-        System.setProperty("spring.mail.password", "test")
+        System.setProperty("aws.ses.access-key", "test")
+        System.setProperty("aws.ses.secret-key", "test")
+        System.setProperty("aws.ses.region", "test")
     }
 }
