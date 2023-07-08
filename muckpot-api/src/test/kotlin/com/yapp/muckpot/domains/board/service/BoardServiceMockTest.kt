3203a11162b1966108265161d88d27cef13eb67f
mountain <pawer02023@gmail.com>
2023-07-09 01:59:19 +0900

    fix: 오늘내일 데이터 오류 개선 (#109)
    - createdAt이 아닌 meetingTime 기준으로 생성되도록 변경

--- muckpot-api/src/test/kotlin/com/yapp/muckpot/domains/board/service/BoardServiceMockTest.kt
+++ muckpot-api/src/test/kotlin/com/yapp/muckpot/domains/board/service/BoardServiceMockTest.kt
@@ -2,6 +2,8 @@ package com.yapp.muckpot.domains.board.service
 
 import Fixture
 import com.ninjasquad.springmockk.MockkBean
+import com.yapp.muckpot.common.constants.TODAY_KR
+import com.yapp.muckpot.common.constants.TOMORROW_KR
 import com.yapp.muckpot.common.dto.CursorPaginationRequest
 import com.yapp.muckpot.domains.board.dto.ParticipantReadResponse
 import com.yapp.muckpot.domains.board.repository.BoardQuerydslRepository
@@ -32,8 +34,8 @@ class BoardServiceMockTest @Autowired constructor(
         val allBoardSize = 3
         val allBoard = listOf(
             Fixture.createBoard(id = 1, title = "board1").apply { meetingTime = LocalDateTime.MIN },
-            Fixture.createBoard(id = 2, title = "board2"),
-            Fixture.createBoard(id = 3, title = "board3")
+            Fixture.createBoard(id = 2, title = "board2").apply { meetingTime = LocalDateTime.now() },
+            Fixture.createBoard(id = 3, title = "board3").apply { meetingTime = LocalDateTime.now().plusDays(1) }
         )
         val participantResponses = listOf(
             ParticipantReadResponse(boardId = 1, userId = 1, nickName = "user1"),
@@ -68,12 +70,12 @@ class BoardServiceMockTest @Autowired constructor(
             actual.list[0].participants.last().nickName shouldBe "외 3명"
         }
 
-        test("현재 시간 이전인 경우 모집마감 으로 바꾸어 응답한다.") {
-
+        test("오늘, 내일은 meetingTime 기준으로 생성된다.") {
             // when
             val actual = boardService.findAllMuckpot(CursorPaginationRequest(null, allBoardSize.toLong()))
             // then
-            actual.list[0].status shouldBe MuckPotStatus.DONE.korNm
+            actual.list[1].todayOrTomorrow shouldBe TODAY_KR
+            actual.list[2].todayOrTomorrow shouldBe TOMORROW_KR
         }
     }
 
