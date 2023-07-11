74272e21255c94910a5ba127ef5be5a223b328d6
taesan <pawer02023@gmail.com>
2023-07-11 19:34:18 +0900

    feat: 직렬화, 역직렬화를 위한 설정추가 - Serializable 구현 - 기본 생성자 사용을 위해 초기 값 지정

--- muckpot-api/src/main/kotlin/com/yapp/muckpot/domains/board/controller/dto/MuckpotCityResponse.kt
+++ muckpot-api/src/main/kotlin/com/yapp/muckpot/domains/board/controller/dto/MuckpotCityResponse.kt
@@ -1,13 +1,14 @@
 package com.yapp.muckpot.domains.board.controller.dto
 
 import com.yapp.muckpot.domains.board.dto.RegionDto
+import java.io.Serializable
 
 data class MuckpotCityResponse(
-    val cityId: Long,
-    val cityName: String,
+    val cityId: Long = 0,
+    val cityName: String = "",
     var sumByCity: Int = 0,
-    val provinces: List<MuckpotProvinceResponse>
-) {
+    val provinces: List<MuckpotProvinceResponse> = emptyList()
+) : Serializable {
     companion object {
         fun of(city: RegionDto.CityDto, provinceDtos: List<RegionDto>): MuckpotCityResponse {
             val provinces = provinceDtos.groupBy { it.province }
--- muckpot-api/src/main/kotlin/com/yapp/muckpot/domains/board/controller/dto/MuckpotProvinceResponse.kt
+++ muckpot-api/src/main/kotlin/com/yapp/muckpot/domains/board/controller/dto/MuckpotProvinceResponse.kt
@@ -1,7 +1,9 @@
 package com.yapp.muckpot.domains.board.controller.dto
 
+import java.io.Serializable
+
 data class MuckpotProvinceResponse(
-    val provinceId: Long,
-    val provinceName: String,
+    val provinceId: Long = 0,
+    val provinceName: String = "",
     val sumByProvince: Int = 0
-)
+) : Serializable
